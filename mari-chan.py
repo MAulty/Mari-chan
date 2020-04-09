df96921c9fabfdf63e0f7d4495f65269f9488894
Jordan Chapman <Nordajnapmach@gmail.com>
2020-04-08 23:05:32 -0700

    Parameter splitting update

--- mari-chan.py
+++ mari-chan.py
@@ -10,16 +10,38 @@ class MyClient(discord.Client):
         globals.defChannel = discord.utils.get(client.get_all_channels(), guild__name='kitty kawk', name='kitty-quarantine')
 
     async def on_message(self, message):
-        print(message.content)
-
         #Don't respond to ourselves
-        words = message.content.split()
         if message.author == self.user:
             return
 
         #Command dispatching
-        if len(words) > 0 and words[0] in globals.commands:
-            await globals.commands[words[0]](words[1:], message)   
+        params = SplitParams(message.content)
+        print(params)
+        if len(params) > 0 and params[0] in globals.commands:
+            await globals.commands[params[0]](params[1:], message)
+
+def SplitParams(str):
+    params = [""]
+    inQuote = False
+
+    #Iterate through string
+    for c in str:
+        #Process differently if in ""
+        if inQuote == False:
+            #Split on space else set quote on quote else add character to latest param
+            if c == ' ' and len(params[-1]) > 0:
+                params.append("")
+            elif c == '"':
+                inQuote = True
+            else:
+                params[-1] += c
+        else:
+            #End quote part if ending quote else just add character
+            if c == '"':
+                inQuote = False
+            else:
+                params[-1] += c
+    return params
 
 client = MyClient()
 client.run(os.environ["Mari-Chan-Key"])
